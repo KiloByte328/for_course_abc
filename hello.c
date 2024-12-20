436f7b18a9f34beee58206a3f72e3da185648c21
KiloByte328 <56058011+KiloByte328@users.noreply.github.com>
2024-12-21 00:02:53 +0700

    Update hello.c x5

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@
 int main (int argc, char** argv)
 {
     printf("Hello, World!\n");
+    printf("Umm.. that was...\n");
     printf("Change da World\n");
     printf("my final messeage\n");
     printf("Goodbye'n");
