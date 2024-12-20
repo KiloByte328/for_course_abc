8aa92009246cae810f935cc52017eb2d1e795ef7
KiloByte328 <56058011+KiloByte328@users.noreply.github.com>
2024-12-21 00:40:45 +0700

    Update hello.c x10

--- hello.c
+++ hello.c
@@ -8,6 +8,7 @@ int main (int argc, char** argv)
     printf("Khm..'\n";
     printf("somwere in... \n");
     printf("Novosibirsk\n");
+    printf("И восстали машины из пепла...\n");
     printf("Change da World\n");
     printf("my final messeage\n");
     printf("Goodbye'n");
